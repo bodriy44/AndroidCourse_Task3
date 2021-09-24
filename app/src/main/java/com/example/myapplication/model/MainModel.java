228aadd319216ff9b4e78f1b352f9bd64af331d2
Бадрединов Эльдар <bodriy666@gmail.com>
2021-09-24 13:24:09 +0400

    Lab3 created NoteAdapter and Note

--- app/src/main/java/com/example/myapplication/model/MainModel.java
+++ app/src/main/java/com/example/myapplication/model/MainModel.java
@@ -1,6 +1,23 @@
 package com.example.myapplication.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainModel {
+    private List<Note> notes;
+
+    public MainModel() {
+        notes = new ArrayList<>();
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+
     public boolean writeToDB(String note) {
         // Добавление в Базу данных
         //...
